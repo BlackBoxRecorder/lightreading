eeeec8d301e01abf8db0d26d0c82124bfcf26042
yinnan <timetickme@outlook.com>
2025-06-11 14:32:52 +0800

    baidutongji

---
+++ public/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?1250177c22b5323bd17712fccbdb476e";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
